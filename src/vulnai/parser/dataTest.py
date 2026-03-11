9c0ff695511c47a858eb588f46f8bc97755079fc
Shayaan <shayaan.hassan1@gmail.com>
2026-03-11 03:57:56 -0700

    Further refined targetExtractor to assign the 'right' side of a var assignment. This will allow for storing the value inside the dict. targetExtractor() now expects 2 parameters. A node's value obj and the target obj are both on the same hierarchical level, so we can safely pass right as a param, regardless of how deep the recursion needs to go to extract the target names. This is not complete, and will break for tuples, augmented assignments, annotated assignments and chained assignments.

--- src/vulnai/parser/dataTest.py
+++ src/vulnai/parser/dataTest.py
@@ -6,5 +6,12 @@ import ast
 
 node = ast.parse("x, y = 1, 2")
 node2 = ast.parse("x = 1")
-print(ast.dump(node, indent=4))
-print(ast.dump(node2, indent=4))
+#print(ast.dump(node, indent=4))
+#print(ast.dump(node2, indent=4))
+
+
+node3 = ast.parse('l[1:2, 3] = y')
+#print(ast.dump(node3,  indent=4))
+
+node4 = ast.parse('snake.colour = w')
+print(ast.dump(node4, indent=4))
--- src/vulnai/parser/python_parser.py
+++ src/vulnai/parser/python_parser.py
@@ -15,11 +15,12 @@ class treeWalk(ast.NodeVisitor):
 
     #Will go through assignment 'styles' that aren't leaf assignments like simple assignment or a subscript assignment
     #It'll then recursively keep traversing into their subtrees until it reaches the leaves.
-    def targetExtractor(self, node):
+    def targetExtractor(self, node, nodeVal):
         if(isinstance(node, ast.Name)):
             self.assignment = {
                 "nodeType": "Name",
-                "target": node.id
+                "target": node.id,
+                "valNode": nodeVal
             }
             self.assignments.append(self.assignment)
             return
@@ -27,13 +28,14 @@ class treeWalk(ast.NodeVisitor):
         elif(isinstance(node, ast.Tuple) or isinstance(node, ast.List)):
             elemList = node.elts
             for elem in elemList:
-                self.targetExtractor(elem)
+                self.targetExtractor(elem, nodeVal)
 
         elif(isinstance(node, ast.Subscript)):
             self.assignment = {
                 "nodeType": "Subscript",
                 "target": node.value,
-                "slice": node.slice
+                "slice": node.slice,
+                "valNode": nodeVal
             }
             self.assignments.append(self.assignment)
 
@@ -42,22 +44,23 @@ class treeWalk(ast.NodeVisitor):
             self.assignment = {
                 "nodeType": "Attribute",
                 "target": node.value,
-                "attribute": node.attr
+                "attribute": node.attr,
+                "valNode": nodeVal
             }
             self.assignments.append(self.assignment)
 
             
         elif(isinstance(node, ast.Starred)):
-            self.targetExtractor(node.value)
+            self.targetExtractor(node.value, nodeVal)
 
     def visit_Assign(self, node):
         left = node.targets #This is a list of nodes
         right = node.value #This is a subtree. Since an assignment can have a bunch of nodes inside it as well
 
-
         for target in left:
-            self.targetExtractor(target)
+            self.targetExtractor(target, right)
 
 
         self.generic_visit(node)
             
+"Further refined targetExtractor to assign the 'right' side of a var assignment. This will allow for storing the value insie the dict. targetExtractor() now expects 2 parameters. A node's value obj and the target obj are both on the same hierarchical level, so we can safely pass right as a param, regardless of how deep the recursion needs to go to extract the target names. This is not complete, and will break for tuples, augmented assignments, annotated assignments and chained assignments."
