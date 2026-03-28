ebae9a979f90e5042b7b3992e697913c01e76d1a
Shayaan <shayaan.hassan1@gmail.com>
2026-03-28 04:53:47 -0700

    Implemented recursive CFG builder with if/else branching and loop back edges

--- src/vulnai/parser/dataTest.py
+++ src/vulnai/parser/dataTest.py
@@ -1,27 +1,31 @@
-#Just a file where I can analyze different types of ASt structures for different nodes
+# #Just a file where I can analyze different types of ASt structures for different nodes
 
 
 
-import ast
+# import ast
 
-node = ast.parse("x, y = 1, 2")
-node2 = ast.parse("x = 1")
-#print(ast.dump(node, indent=4))
-#print(ast.dump(node2, indent=4))
-
-
-node3 = ast.parse('l[1:2, 3] = y')
-#print(ast.dump(node3,  indent=4))
-
-node4 = ast.parse('snake.colour = w')
-#print(ast.dump(node4, indent=4))
-
-node5 = ast.parse('def func(a,b): return 0')
-#print(ast.dump(node5, indent =4))
+# node = ast.parse("x, y = 1, 2")
+# node2 = ast.parse("x = 1")
+# #print(ast.dump(node, indent=4))
+# #print(ast.dump(node2, indent=4))
 
 
-# def randoFuncToVisualizeCFG():
-#     a = 5
-#     b = 10
-#     return a + b
+# node3 = ast.parse('l[1:2, 3] = y')
+# #print(ast.dump(node3,  indent=4))
 
+# node4 = ast.parse('snake.colour = w')
+# #print(ast.dump(node4, indent=4))
+
+# node5 = ast.parse('def func(a,b): return 0')
+# #print(ast.dump(node5, indent =4))
+
+
+def randoFuncToVisualizeCFG(x = 0, y = 0, z = 0):
+    if x == 0:
+        x + 1
+    elif y==0:
+        y + 1
+    else:
+        z + 1
+
+randoFuncToVisualizeCFG(x=0, y =0, z = 0)
